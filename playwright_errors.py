c93aab5c9cd2c7dced310f322333645c451ef243
uicoursestudy <4everundead4everhu@gmail.com>
2026-05-13 16:32:12 +0300

    Работа с авторизацией Playwright

--- playwright_errors.py
+++ playwright_errors.py
@@ -9,14 +9,14 @@ with sync_playwright() as playwright:
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login',
               wait_until='networkidle')
 
-#    unknown = page.locator('#unknown')
-#    expect(unknown).to_be_visible()
+    # unknown = page.locator('#unknown')
+    # expect(unknown).to_be_visible()
 
-#    login_button = page.get_by_test_id('login-page-login-button')
-#    login_button.fill('unknown')
+    # login_button = page.get_by_test_id('login-page-login-button')
+    # login_button.fill('unknown')
 
-    page.evaluate("""
-                     const title = document.getElementById('authentication-ui-course-title-text')
-                     title.textContent = 'New Text'
-                 """
-                  )
+    # page.evaluate("""
+    #                  const title = document.getElementById('authentication-ui-course-title-text')
+    #                  title.textContent = 'New Text'
+    #              """
+    #               )
