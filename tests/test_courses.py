cb3f32439d08cef2c1d041bf37a76472973d633b
uicoursestudy <4everundead4everhu@gmail.com>
2026-05-18 17:46:04 +0300

    Маркировки Pytest

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,9 +1,6 @@
-from playwright.sync_api import sync_playwright
-import pytest
+from playwright.sync_api import sync_playwright, expect
 
-@pytest.mark.regression
-@pytest.mark.registration
-def test_successful_registration():
+def test_empty_courses_list():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
         context = browser.new_context()
@@ -23,6 +20,10 @@ def test_successful_registration():
         registration_button = page.get_by_test_id('registration-page-registration-button')
         registration_button.click()
 
+        title = page.get_by_test_id('dashboard-toolbar-title-text')
+        expect(title).to_be_visible()
+        expect(title).to_have_text('Dashboard')
+
         context.storage_state(path='browser-state.json')
 
     with sync_playwright() as playwright:
@@ -30,6 +31,19 @@ def test_successful_registration():
         context = browser.new_context(storage_state='browser-state.json')
         page = context.new_page()
 
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
-        page.wait_for_timeout(5000)
+        title = page.get_by_test_id('courses-list-toolbar-title-text')
+        expect(title).to_be_visible()
+        expect(title).to_have_text('Courses')
+
+        results_icon = page.get_by_test_id('courses-list-empty-view-icon')
+        expect(results_icon).to_be_visible()
+
+        results_block = page.get_by_test_id('courses-list-empty-view-title-text')
+        expect(results_block).to_be_visible()
+        expect(results_block).to_have_text('There is no results')
+
+        results_text = page.get_by_test_id('courses-list-empty-view-description-text')
+        expect(results_text).to_be_visible()
+        expect(results_text).to_have_text('Results from the load test pipeline will be displayed here')
